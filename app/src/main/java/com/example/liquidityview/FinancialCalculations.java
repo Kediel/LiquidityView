30badda2107c78d336eb9feed1704b805c7de69c
Kediel <km3556@drexel.edu>
2018-04-14 12:35:20 -0400

    Added Financial Calculations java file.
    I created a java file that will perform all financial calculations.

---
+++ app/src/main/java/com/example/liquidityview/FinancialCalculations.java
@@ -0,0 +1,32 @@
+package com.example.liquidityview;
+
+public class FinancialCalculations {
+
+    void Debt(){
+
+        // Finish me
+    }
+
+    void Interest(){
+        // Finish me
+    }
+
+    void Loans(){
+        // Finish me
+
+    }
+
+    void Insurance(){
+        // Finish me
+    }
+
+    void Retirement(){
+
+        // Finish me
+    }
+
+    void Taxes(){
+        // Finish me
+    }
+
+}
