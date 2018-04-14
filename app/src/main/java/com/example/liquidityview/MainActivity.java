8fc87ea3be5f49c662ffeea08b41e378f14ff908
Yusuf Kerem Ozaydin <ykerem99@gmail.com>
2018-04-14 18:36:30 -0400

    Reupdate

---
+++ app/src/main/java/com/example/liquidityview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.liquidityview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    float Expenses[] = {77, 20.5f, 106.3f, 67.25f, 12, 59.8f };
+
+    String Categories[] = { "Food", "Travel", "Miscellaneous", "Transportation", "Shopping", "Entertainment"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupPieChart();
+
+    }
+
+    private void setupPieChart() {
+
+        List<PieEntry> pieEntries = new ArrayList<>();
+
+        for (int i = 0; i < Expenses.length; i++) {
+            pieEntries.add(new PieEntry(Expenses[i], Categories[i]));
+        }
+
+        PieDataSet dataSet = new PieDataSet(pieEntries, "Budget This Month");
+        PieData data = new PieData(dataSet);
+
+        PieChart chart = (PieChart) findViewById(R.id.chart);
+        chart.setData(data);
+        chart.invalidate();
+    }
+
+}
